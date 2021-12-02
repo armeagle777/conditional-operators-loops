32bd8145448bc035829f6d4f2e25fdfd3290ec9d
armeagle777 <tyeranyan@gmail.com>
2021-12-02 16:46:41 +0400

    feat: Is array copied task resoled

---
+++ classRoomTasks/isArrayCopied.js
@@ -0,0 +1,10 @@
+//TODO: What is this code ( fruits.length ) going to show?
+
+let fruits = ['Apples', 'Pear', 'Orange'];
+
+//*@ pushing a new value into 'copy'
+let shoppingCart = fruits;
+shoppingCart.push('Banana');
+
+//? Testing the fruits length 
+console.log(`fruit's length now is : ${fruits.length}`); //! the output will be 4 
